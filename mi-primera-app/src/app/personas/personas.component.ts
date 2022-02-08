70a053183750a3978dc4afe567961d122892e683
Stiv <strodriguezna@gmail.com>
2022-02-07 21:03:21 -0500

    Se agrega la funcionalidad de ngIf con Else

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-personas',
-  templateUrl: './personas.component.html',
-  styles: []
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrls: ['./personas.component.css'] 
 })
-export class PersonasComponent  {
-  deshabilitar:boolean = false;
-  mensaje:string ='No se ha agregado ninguna persona';
-  titulo:string = '';
+export class PersonasComponent{
+    deshabilitar = false;
+    mensaje = '';
+    titulo = 'Ingeniero';
+    mostrar = false;
 
-  agregarPersona(){
-    this.mensaje = 'Persona agregada';
-  }
+    agregarPersona(){
+        this.mostrar = true;
+        this.mensaje = 'Persona agregada';
+    }
 
-  modificarTitulo(event: Event){
-    this.titulo = (<HTMLInputElement>event.target).value;
-  }
 }
